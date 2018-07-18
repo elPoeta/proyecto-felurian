edbb139839afc0303ed3359e0305dc2c8b7c07f6
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2018-07-18 21:32:17 +0000

    Sitio Responsive

--- js/menu.js
+++ js/menu.js
@@ -26,17 +26,43 @@ class Menu{
 
 let m = new Menu;
 m.add(categorias);
-//m.viewMenu('#panel-side-menu');
+m.viewMenu('#panel-side-menu');
 m.viewMenu('#menu-template');
 
 function menuEventos(){
     let categoria = document.querySelectorAll('.categoria');
                      for (let i = 0; i < categoria.length; i++) {
                         categoria[i].addEventListener('touchstart', ()=> {
-                            let id = categoria[i].getAttribute('id');
-                            id = id.replace(/\D/g,'');
-                            buscarPorCategoria(id);
+                            eventoBuscar(categoria, i);
                         });
-                      
+                        categoria[i].addEventListener('click', ()=> {
+                            eventoBuscar(categoria,i);
+                        });
                       }
+
 }
+
+function eventoBuscar(categoria, i){
+    let id = categoria[i].getAttribute('id');
+    id = id.replace(/\D/g,'');
+    buscarPorCategoria(id);
+  }
+
+function buscarPorCategoria(id){
+    if(id == 1){
+      cargarProductos();
+    }
+}
+
+const btnNavIcon = document.querySelector('#nav-icon-hamburger');
+const btnNavPanel = document.querySelector('#btn-nav-panel'); 
+
+btnNavIcon.addEventListener('touchstart', (e)=>{
+    e.preventDefault();
+    document.querySelector('.nav-panel').style.width='250px';
+});
+
+btnNavPanel.addEventListener('touchstart', (e)=>{
+    e.preventDefault();
+    document.querySelector('.nav-panel').style.width='0px';
+});
