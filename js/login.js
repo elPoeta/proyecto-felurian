6c9a1547cdb8ee78fd4834bba21ee20b968ab510
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2018-09-29 19:03:19 -0300

    nuevo login

--- js/login.js
+++ js/login.js
@@ -1,25 +1,79 @@
-// Get modal element
-const modal = document.querySelector('#simpleModal');
-// Get open modal button
-const modalBtn = document.querySelector('.login');
-// Get close button
-const closeBtn = document.querySelector('#closeBtn');
 
-// Listen for open click
-modalBtn.addEventListener('click', openModal);
-// Listen for close click
-closeBtn.addEventListener('click', closeModal);
-// Listen for outside click
-//window.addEventListener('click', outsideClick);
+let logEmail = document.querySelector('#log-email');
+let logPass = document.querySelector('#log-pass');
+let btnLogin = document.querySelector('#btn-login');
+const URL_LOGIN_SERVER = 'LoginServer';
 
-// Function to open modal
-function openModal(){
-  modal.style.display = 'block';
-  document.querySelector('body').style.backgroundColor='#00000080';
+
+const  loginMsg = document.querySelector('.loginMsg');
+const  loginFront = document.querySelector('.login-front');
+const  signupMsg = document.querySelector('.signupMsg');
+const  signup = document.querySelector('.signup');
+const  frontbox = document.querySelector('.frontbox');
+const  switch1 = document.querySelector('#switch1');
+const  switch2 = document.querySelector('#switch2'); 
+
+switch1.addEventListener('click',sw1);
+
+function sw1() {
+    loginMsg.classList.toggle("visibility");
+    frontbox.classList.add("moving");
+    signupMsg.classList.toggle("visibility");
+  
+    signup.classList.toggle('hide');
+    loginFront.classList.toggle('hide');
 }
 
-// Function to close modal
-function closeModal(){
-  modal.style.display = 'none';
-  document.querySelector('body').style.backgroundColor= 'none';
+switch2.addEventListener('click', sw2);
+
+function sw2() {
+    loginMsg.classList.toggle("visibility");
+    frontbox.classList.remove("moving");
+    signupMsg.classList.toggle("visibility");
+  
+    signup.classList.toggle('hide');
+    loginFront.classList.toggle('hide');
+  }
+//
+//setTimeout(function(){
+//  sw1();
+//},1000);
+//
+//setTimeout(function(){
+//  sw2();
+//},3000);
+
+
+btnLogin.addEventListener('click', (e)=>{
+     console.log('log >',e);
+   let loginUsuario ={};
+   loginUsuario.email = logEmail.value;
+   loginUsuario.password = logPass.value;
+   console.log(loginUsuario);
+
+   Http.post(URL_LOGIN_SERVER,loginUsuario)
+           .then(response => response.json())
+  .then( data => {
+       console.log('log >>> ',data);
+           if(data !== null && data !== 'error'){
+                location.replace("index.html");
+           }else{
+                      msgLogueo("Error al iniciar sesion",'msg-color-error');
+
+           }
+                    
+    }).catch (err => {
+       console.log("error",err);
+       msgLogueo("Error al iniciar sesion "+err,'msg-color-error');
+});
+  
+});
+
+function msgLogueo(msg,color){
+    let template = 
+            `<div class="mensaje-login">
+                 <p class="${color}">${msg}</p>
+             </div>`;
+    document.querySelector('#panel-login-msg').innerHTML=template;
+    
 }
